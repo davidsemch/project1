198c90c12bf9e7e069bf1ebc425c0f07ed3a5ddf
080917 <davidsemchishin@gmail.com>
2023-03-08 14:39:50 -0500

    sdfg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,64 +11,150 @@ public class Main {
     static Scanner input = new Scanner(System.in);  // Create a Scanner object
 
     public static void main(String[] args) {
-        ArrayList<String> myList = new ArrayList<String>();
-        System.out.println("(1) Add a task.");
-        System.out.println("(2) Remove a task.");
-        System.out.println("(3) Update a task.");
-        System.out.println("(4) List all tasks.");
-        System.out.println("(0) Exit.");
-        System.out.println("Please make your choice: ");
+        ArrayList<Task> myList = new ArrayList<Task>();
+        try {
+            System.out.println("(1) Add a task.");
+            System.out.println("(2) Remove a task.");
+            System.out.println("(3) Update a task.");
+            System.out.println("(4) List all tasks.");
+            System.out.println("(5) Find by priority");
+            System.out.println("(0) Exit.");
+            System.out.println("Please make your choice: ");
 
-        int userInput = input.nextInt();
+            int userInput = input.nextInt();
+            input.nextLine();
+
+
+            while (true) {
+                if (userInput == 1) {
+                    addTask(myList);
+
+                } else if (userInput == 2) {
+                    removeTask(myList);
+
+                } else if (userInput == 3) {
+                    updateTask(myList);
+
+                } else if (userInput == 4) {
+                    System.out.println(myList);
+
+                } else if (userInput == 0) {
+                    System.exit(0);
+                } else if (userInput == 5) {
+                    prioCheck(myList);
+                }
+
+
+                System.out.println("Please make your choice: ");
+                userInput = input.nextInt();
+                input.nextLine();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Sorry, there was an error in your code somewhere!");
+        }
+    }
+    static ArrayList<Task> addTask(ArrayList<Task> a) {
+        System.out.println("Please enter the title of the task you'd like to add: ");
+        String title = input.nextLine();
+
+        System.out.println("Please enter the description of the task you'd like to add: ");
+        String desc = input.nextLine();
+
+        System.out.println("Please enter the priority of the task you'd like to add: ");
+        int prio = input.nextInt();
+
         input.nextLine();
 
+        Task aNewTask = new Task(title, desc, prio);
 
-        while (true) {
-            if (userInput == 1) {
-                addTask(myList);
-
-            } else if (userInput == 2) {
-                removeTask(myList);
-
-            } else if (userInput == 3) {
-                updateTask(myList);
-
-            } else if (userInput == 4) {
-                System.out.println(myList);
-
-            } else if (userInput == 0) {
-                System.exit(0);
-            }
-            System.out.println("Please make your choice: ");
-            userInput = input.nextInt();
-            input.nextLine();
-        }
-
-    }
-
-    static ArrayList<String> addTask(ArrayList<String> a) {
-        System.out.println("Please enter task you'd like to add: ");
-        String task = input.nextLine();
-        a.add(task);
+        a.add(aNewTask);
         return a;
     }
 
-    static ArrayList<String> removeTask(ArrayList<String> a) {
-        System.out.println("Please enter the task you'd like to remove: ");
+    static ArrayList<Task> removeTask(ArrayList<Task> a) {
+        System.out.println("Please enter the index of the task you'd like to remove: ");
         String task = input.nextLine();
-        a.remove(task);
+        a.remove(Integer.parseInt(task));
         return a;
     }
 
-    static ArrayList<String> updateTask(ArrayList<String> a) {
+    static ArrayList<Task> updateTask(ArrayList<Task> a) {
         System.out.println("What index would you like to replace: ");
         int ind = input.nextInt();
         input.nextLine();
 
-        System.out.println("What would you like to update it to: ");
-        String update = input.nextLine();
+        System.out.println("Please enter the title of the task you'd like to add: ");
+        String title = input.nextLine();
 
-        a.set(ind, update);
+        System.out.println("Please enter the description of the task you'd like to add: ");
+        String desc = input.nextLine();
+
+        System.out.println("Please enter the priority of the task you'd like to add: ");
+        int prio = input.nextInt();
+
+        input.nextLine();
+
+        Task aNewTask = new Task(title, desc, prio);
+
+        a.set(ind, aNewTask);
         return a;
+    }
+
+    static ArrayList<Task> prioCheck(ArrayList<Task>tasks){
+        System.out.println("What priority task do you want to find?");
+        for(Task item:tasks){
+            int userInput = input.nextInt();
+            int prio = item.getPriority();
+            if(prio == userInput){
+                System.out.println(item);
+            }
+        }
+        return tasks;
+    }
+}
+
+class Task{
+    private String title;
+    private String description;
+    private int priority;
+
+    public Task(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", priority=" + priority +
+                '}';
     }
 }
